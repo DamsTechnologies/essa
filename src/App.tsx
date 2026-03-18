20a6d923cf6a6de0908916d3f2ea19024c04ebf1
DAMS TECHNOLOGIES <damstech00@gmail.com>
2026-03-18 17:53:34 +0000

    Replace FashionContest and ContestantDetail with CompetitionRedirect

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import EventDetail from "./pages/EventDetail";
 import EventContestantDetail from "./pages/EventContestantDetail";
 import NotFound from "./pages/NotFound";
 import ScrollToTop from "./components/ScrollToTop";
+import CompetitionRedirect from "@/pages/CompetitionRedirect";
 
 const queryClient = new QueryClient();
 
@@ -39,8 +40,8 @@ const App = () => (
           <Route path="/executives" element={<Executives />} />
           <Route path="/student-life" element={<StudentLife />} />
           <Route path="/events" element={<Events />} />
-          <Route path="/competition" element={<FashionContest />} />
-          <Route path="/contestant/:slug" element={<ContestantDetail />} />
+          <Route path="/competition" element={<CompetitionRedirect />} />
+          <Route path="/contestant/:slug" element={<CompetitionRedirect />} />
           <Route path="/events/fashion-contest" element={<Navigate to="/competition" replace />} />
           <Route path="/constitution" element={<Constitution />} />
           <Route path="/welfare" element={<Welfare />} />
